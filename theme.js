3419f132d6e15f0b92fa7c83db284b74565d9693
brawler07 <trignosharma001@gmail.com>
2022-04-18 13:25:46 +0530

    ...

---
+++ theme.js
@@ -0,0 +1,11 @@
+function changeTheme(){
+    let theme=document.getElementById("body");
+    theme.style.backgroundColor="Black";
+    
+}
+
+function xchangeTheme(){
+    let theme=document.getElementById("body");
+    theme.style.backgroundColor="white";
+    
+}
